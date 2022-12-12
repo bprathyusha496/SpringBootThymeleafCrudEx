29da542dd8fe701effdcdc452c74f49e915516ac
Prathyusha Bashaveni <prathyusha.bashaveni@ratnaglobaltech.com>
2022-12-12 15:21:35 +0530

    update sysout

--- src/main/java/com/net/SpringBootThymeleafCrudExApplication.java
+++ src/main/java/com/net/SpringBootThymeleafCrudExApplication.java
@@ -8,6 +8,7 @@ public class SpringBootThymeleafCrudExApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafCrudExApplication.class, args);
+		System.out.println("jenkings with github...");
 	}
 
 }
