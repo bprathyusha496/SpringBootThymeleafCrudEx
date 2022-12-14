566344b0a03fc2f6aa4869f4c0c2c0b7d9ffbca7
Prathyusha Bashaveni <prathyusha.bashaveni@ratnaglobaltech.com>
2022-12-14 17:28:26 +0530

    war commit

--- src/main/java/com/net/Controller/StudentController.java
+++ src/main/java/com/net/Controller/StudentController.java
@@ -10,11 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.net.Model.Student;
 import com.net.Service.StudentService;
 
-@Controller
+@RestController
 public class StudentController {
 	@Autowired
 	private StudentService studentservice;
