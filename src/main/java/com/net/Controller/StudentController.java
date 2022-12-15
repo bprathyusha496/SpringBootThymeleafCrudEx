c62fc6c99cc8bb0ec90006af3f28a5012d562bef
Prathyusha Bashaveni <prathyusha.bashaveni@ratnaglobaltech.com>
2022-12-15 15:41:17 +0530

    commit

--- src/main/java/com/net/Controller/StudentController.java
+++ src/main/java/com/net/Controller/StudentController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.net.Model.Student;
 import com.net.Service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 	@Autowired
 	private StudentService studentservice;
