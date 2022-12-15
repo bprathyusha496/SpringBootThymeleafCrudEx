53a9bc7b865c8c174ce6881c830cf8e75f13e4d9
Prathyusha Bashaveni <prathyusha.bashaveni@ratnaglobaltech.com>
2022-12-15 15:25:41 +0530

    main method changes

--- src/main/java/com/net/SpringBootThymeleafCrudExApplication.java
+++ src/main/java/com/net/SpringBootThymeleafCrudExApplication.java
@@ -2,13 +2,17 @@ package com.net;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@ComponentScan("com.net.*")
-
-public class SpringBootThymeleafCrudExApplication {
-
+public class SpringBootThymeleafCrudExApplication extends SpringBootServletInitializer {
+	
+@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder initializer) {
+	  return initializer.sources(SpringBootThymeleafCrudExApplication.class);
+		
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafCrudExApplication.class, args);
 		System.out.println("jenkings with github...");
